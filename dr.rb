5394f66f2ef4e4787c9a27e98d8e94a34b8d759e
Nick McDonnough <nickmcd@gmail.com>
2014-09-24 08:55:53 -0500

    Initial Commit

---
+++ dr.rb
@@ -0,0 +1,11 @@
+# A digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has two
+# digits, continue reducing in this way until a single-digit number
+# is produced. This is only applicable to the real numbers and zero.
+
+# Make the tests pass.
+
+
+module DigitalRoot
+
+end
